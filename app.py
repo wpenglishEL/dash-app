99ab2be925af0e58517b20d4980dfee3dc0c94b7
wpenglishEL <66124122+wpenglishEL@users.noreply.github.com>
2020-07-21 11:24:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -237,10 +237,10 @@ def generate_results(
         repo = "wpenglish/trackingJSON"
         g = Github("730b20add" + "112f61ecc5" + "b89fa43fd" + "d8123fb4bb44")
         repo = g.get_repo(repo)
-	data_set = {"name": user_name, "temp": [temperature], "symptoms":[symptoms], "rating":[feeling_rating], "water_intake": [water_intake], "Soup":[soup]}
-	json_dump = json.dumps(data_set)
-	repo.create_file(user_name + ".json", user_name + " data", json_dump)
-    return html.P("")
+        data_set = {"name": user_name, "temp": [temperature], "symptoms":[symptoms], "rating":[feeling_rating], "water_intake": [water_intake], "Soup":[soup]}
+        json_dump = json.dumps(data_set)
+        repo.create_file(user_name + ".json", user_name + " data", json_dump)
+        return html.P("")
 
 if __name__ == '__main__':
-    app.run_server(port=8888, host='0.0.0.0', debug=True)
+    app.run_server(debug=True)
